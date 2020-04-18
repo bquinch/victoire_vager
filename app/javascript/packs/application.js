a5e1738a707e0dbc667cb98caa422b78ee0ef2bc
Basile Quinchon <basile.quinchon@gmail.com>
2020-04-18 21:21:21 +0200

    relax

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import "flatpickr/dist/themes/airbnb.css";
+import { initRellax } from "plugins/init_rellax.js";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { initFlatPickr } from 'plugins/init_flatpickr.js';
@@ -7,6 +8,10 @@ import { navColor } from 'plugins/nav_color.js';
 
 if (document.querySelector('.datepicker')) {
   initFlatPickr();
+};
+
+if (document.querySelector('.rellax')) {
+  initRellax();
 };
 
 if (document.querySelector('.navbar')) {
