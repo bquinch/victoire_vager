a52b894db91d77e7e2bfd67550adc771a7e90e66
Basile Quinchon <basile.quinchon@gmail.com>
2020-04-22 14:15:52 +0200

    validation name

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ApplicationRecord
-  validates :description, :priority, :category, presence: true
+  validates :name, :priority, :category, presence: true
   validates :category, inclusion: { in: ["Communication", "Technologies", "Soft Skills"] }
 end
