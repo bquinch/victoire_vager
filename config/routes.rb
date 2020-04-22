d44d78475a505f50250ec7dee9993f744cccdc59
Basile Quinchon <basile.quinchon@gmail.com>
2020-04-22 14:54:01 +0200

    delete useless routes and files

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :messages, only: :create
-  resources :experiences, only: %i[index new create edit update destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
